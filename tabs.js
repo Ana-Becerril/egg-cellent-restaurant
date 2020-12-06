e0daf0c597590e6bb7f8617cc1f598b1d6ca4356
Ana- Becerril <ana.becbel@gmail.com>
2020-12-05 19:03:26 -0600

    positioned and styled the tabs into the home page

--- tabs.js
+++ tabs.js
@@ -2,19 +2,28 @@
 function tabs(){
 const parent=document.getElementById("parent")
 
+const tabsContainer=document.createElement("div");
+tabsContainer.classList.add("tabs-container");
+
 const homeTab=document.createElement("div");
 homeTab.innerHTML="Home"
 homeTab.classList.add("home-tab");
+
 const menuTab=document.createElement("div");
 menuTab.innerHTML="Menu"
 menuTab.classList.add("menu-tab");
+
 const shopTab=document.createElement("div");
 shopTab.innerHTML="Shop"
 shopTab.classList.add("shop-tab");
+
 const aboutUsTab=document.createElement("div");
 aboutUsTab.innerHTML="About Us"
 aboutUsTab.classList.add("aboutUs-tab");
-parent.append(homeTab, menuTab, shopTab, aboutUsTab)
+
+tabsContainer.append(homeTab, menuTab, shopTab, aboutUsTab);
+parent.appendChild(tabsContainer);
+
 }
 
 export default tabs;
