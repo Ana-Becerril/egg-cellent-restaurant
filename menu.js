6ddbe723ceeb3db9f2b4f5f034c15d7d442fbdfb
Ana- Becerril <ana.becbel@gmail.com>
2020-12-17 19:25:10 -0600

    made a new branch named 'menu', in wich made the bg and added some sections

---
+++ menu.js
@@ -0,0 +1,32 @@
+function menu(){
+
+    const menuContainer=document.createElement("div");
+    menuContainer.classList.add("menu-container");
+
+    const menuLeft=document.createElement("div");
+    menuLeft.classList.add("menu-left");
+
+    const menuMiddle=document.createElement("div");
+    const menuStarters=document.createElement("div")
+    menuMiddle.classList.add("menu-middle");
+    menuStarters.innerHTML="STARTERS"
+    menuStarters.classList.add("menu-tittle");
+    const menuSpecials=document.createElement("div")
+    menuSpecials.innerHTML="SPECIALS"
+    menuSpecials.classList.add("menu-specials");
+    const menuDrinks=document.createElement("div")
+    menuDrinks.innerHTML="DRINKS"
+    menuDrinks.classList.add("menu-drinks")
+
+    menuMiddle.append(menuStarters, menuSpecials, menuDrinks)
+
+    const menuRight=document.createElement("div");
+    menuRight.classList.add("menu-right");
+
+    menuContainer.append(menuLeft,menuMiddle, menuRight);
+
+
+return menuContainer;
+
+}
+export default menu;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import './style.css';
 import tabs from "../tabs"
 import home from "../home"
 import Image from './home-image.jpg';
+import menu from '../menu';
 
 
 const parent=document.getElementById("parent")
-parent.append(tabs(), home());
+parent.append(tabs(), menu());
 
 const myImage = document.createElement("img");
 myImage.src = Image;
