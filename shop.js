d89a8cb04024dadc1bf3db2c421064b8936d9bd6
Ana- Becerril <ana.becbel@gmail.com>
2021-01-12 13:33:20 -0600

    added options to the body of the menu for shop, added button to add to cart and styled the header to improve the contrast

--- shop.js
+++ shop.js
@@ -12,8 +12,41 @@ function shop(){
     const bodyShop=document.createElement("div");
     bodyShop.classList.add("body-shop");
 
+    const itemsParent=document.createElement("div");
+    itemsParent.classList.add("items-parent")
 
+    const itemOneShop=document.createElement("div");
+    itemOneShop.classList.add("item-one-shop");
+    itemOneShop.append(addElementsToListShop("IN FAMILY", "4 eggs (to choose), 4 breads, 4 drinks", "ADD TO CART"));
+
+    const itemTwoShop=document.createElement("div");
+    itemTwoShop.classList.add("item-two-shop");
+    itemTwoShop.append(addElementsToListShop("FOR SHARE", "2 eggs (to choose), 2 breads, 2 drinks", "ADD TO CART"));
+
+    const itemThreeShop=document.createElement("div");
+    itemThreeShop.classList.add("item-three-shop");
+    itemThreeShop.append(addElementsToListShop("ÑAM", "1 eggs (to choose), 1 breads, 1 drinks", "ADD TO CART"));
+
+    itemsParent.append(itemOneShop,itemTwoShop,itemThreeShop);
+    bodyShop.appendChild(itemsParent);
     shopContainer.append(headerShop, bodyShop);
+
+    function addElementsToListShop(tittle, description,button){
+        const optionShop=document.createElement("div")
+        optionShop.classList.add("option-shop")
+        const tittleListShop=document.createElement("div")
+        tittleListShop.innerHTML=tittle;
+        tittleListShop.classList.add("options-tittle-shop");
+        const listShop=document.createElement("div");
+        listShop.innerHTML=description;
+        listShop.classList.add("options-description-shop");
+        const buttonShop=document.createElement("button");
+        buttonShop.innerHTML=button;
+        buttonShop.classList.add("button-shop");
+        optionShop.append(tittleListShop,listShop,buttonShop);
+        return optionShop;
+    }
+
     return shopContainer;
 
 }
